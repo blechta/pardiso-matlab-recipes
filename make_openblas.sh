19f9f76c4f03843565df0b6e92a75f53ec0c769a
Jan Blechta <blechta@karlin.mff.cuni.cz>
2018-06-29 12:40:45 +0200

    Remove `make -j` option ignored by OpenBLAS
    It actually uses make `MAKE_NB_JOBS=...` and sets the default to
     number of cores. User can set this manually if they need to.

--- make_openblas.sh
+++ make_openblas.sh
@@ -2,7 +2,6 @@
 set -e
 
 # Default values
-: ${PROCS:=1}
 : ${OPENBLAS_VERSION:=0.3.0}
 : ${PREFIX:="${PWD}/build"}
 
@@ -12,5 +11,5 @@ tar -xzf OpenBLAS-${OPENBLAS_VERSION}.tar.gz
 
 # Build and install OpenBLAS into the build dir
 cd OpenBLAS-${OPENBLAS_VERSION}
-make -j${PROCS} NO_LAPACKE=1 NO_CBLAS=1 USE_OPENMP=0 USE_THREAD=0
+make NO_LAPACKE=1 NO_CBLAS=1 USE_OPENMP=0 USE_THREAD=0
 make install NO_LAPACKE=1 NO_CBLAS=1 NO_STATIC=1 PREFIX="${PREFIX}"
