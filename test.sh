bd940525bb67c9a82d80ae028bc96f25b9d6302b
Jan Blechta <blechta@karlin.mff.cuni.cz>
2018-06-29 13:37:44 +0200

    Allow different matlab binaries

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 set -e
 
 # Default values
+: ${MATLAB:=matlab}
 : ${PREFIX:="${PWD}/build"}
 
 # Load needed environment setup
@@ -10,4 +11,4 @@ source "${PREFIX}/share/pardiso_wrappers.conf"
 
 # Tell matlab where to search for the examples and run them
 export MATLABPATH="${PWD}/pardiso-matlab:${MATLABPATH}"
-matlab -nojvm -nodesktop -r "examplesym; exampleunsym; exampleherm; examplecomplex; exit"
+${MATLAB} -nojvm -nodesktop -r "examplesym; exampleunsym; exampleherm; examplecomplex; exit"
